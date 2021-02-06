250f1806d372c4c727717ee0cf5dac7bd1d6de4a
Mark Ustyugov <mark.ustyugov@gmail.com>
2021-02-06 20:33:21 +0500

    Добавлены функции для генерирования объектов

--- js/main.js
+++ js/main.js
@@ -1,4 +1,43 @@
-const getRandomNumber = function (min, max) {
+'use strict';
+
+const PHOTO_DESCRIPTION_COUNT = 25;
+const URL_PHOTO_PATH = 'photo/';
+const AVATAR_PATH = 'img/avatar-';
+
+const PHOTO_DESCRIPTION = [
+  'Description-1',
+  'Description-2',
+  'Description-3',
+  'Description-4',
+  'Description-5',
+  'Description-6',
+];
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const NAMES = [
+  'Андрей',
+  'Пётр',
+  'Степан',
+  'Иван',
+  'Борис',
+  'Анастасия',
+  'Елена',
+  'Алёна',
+  'Екатерина',
+  'Мария',
+];
+
+const [jpg, svg] = ['.jpg', '.svg'];
+
+const getRandomNumber = function(min, max) {
   if (min < 0 || max < 0) {
     alert('Вы ввели отрицательное число. Допускаются только положительные числа.');
   } else if (min >= max) {
@@ -7,11 +46,73 @@ const getRandomNumber = function (min, max) {
     let rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
   }
-}
+};
 
-const getMaxCharLength = function (string, maxLength) {
+const getRandomArrayElement = function(elements) {
+  return elements[getRandomNumber(0, elements.length - 1)];
+};
+
+const getMaxCharLength = function(string, maxLength) {
   return string.length <= maxLength ? true : false;
-}
+};
 
-getMaxCharLength('Some text', 140);
-getRandomNumber(0, 10);
+const createPhotoDescription = function() {
+  const array = new Array();
+  let uniqueId = 1;
+  for(let i = 0; i < PHOTO_DESCRIPTION_COUNT; i++) {
+    array[i] = {
+      id: uniqueId,
+      url: URL_PHOTO_PATH + uniqueId + jpg,
+      description: getRandomArrayElement(PHOTO_DESCRIPTION),
+      likes: getRandomNumber(100, 250),
+      comments: createComment(),
+    }
+    uniqueId++;
+  }
+  return array;
+};
+
+const createComment = function() {
+  const array = new Array();
+  let uniqueId = 1;
+  for(let i = 0; i < getRandomNumber(1, 10); i++) {
+    array[i] = {
+      id: uniqueId,
+      avatar: AVATAR_PATH + getRandomNumber(1, 6) + svg,
+      message: getRandomArrayElement(MESSAGES),
+      name: getRandomArrayElement(NAMES),
+    }
+    uniqueId++;
+  }
+  return array;
+};
+
+const photoDescreption = createPhotoDescription();
+photoDescreption;
+getMaxCharLength(1, 140);
+
+// const createPhotoDescription = function() {
+//   return {
+//     id: uniquePhotoDescriptionId++,
+//     url: URL_PHOTO_PATH + uniqueUrl++ + jpg,
+//     description: getRandomArrayElement(PHOTO_DESCRIPTION),
+//     likes: getRandomNumber(100, 250),
+//     comments: new Array(getRandomNumber(1, 10)).fill(null).map(() => createComment()),
+//   }
+// };
+
+// const createComment = function() {
+//   return {
+//     id: uniqueCommentId++,
+//     avatar: AVATAR_PATH + getRandomNumber(1, 6) + svg,
+//     message: getRandomArrayElement(MESSAGES),
+//     name: getRandomArrayElement(NAMES),
+//   }
+// };
+
+// let uniquePhotoDescriptionId = 1;
+// let uniqueUrl = 1;
+// let uniqueCommentId = 1;
+
+// const descriptions = new Array(PHOTO_DESCRIPTION_COUNT).fill(null).map(() => createPhotoDescription());
+// console.log(descriptions);
