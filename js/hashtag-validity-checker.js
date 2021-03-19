08ebf829330ec1a80b7ffb2e8ff3b38c74a63e8b
Mark Ustyugov <mark.ustyugov@gmail.com>
2021-03-19 21:59:52 +0500

    исправил замечания

--- js/hashtag-validity-checker.js
+++ js/hashtag-validity-checker.js
@@ -1,48 +1,30 @@
 const hashtagInput = document.querySelector('.text__hashtags');
 
-const REGEXP = /^#{1}[a-zа-я0-9]{1,19}$/i;
+const REGEXP = /^#[a-zа-я0-9]{1,19}$/i;
 const MAX_HASHTAG_NUMBER = 5;
-let hashtagsArray = new Array();
 
 const hashtagErrorChecker = (inputArray) => {
   const arrayLength = inputArray.length;
-
-  let patternErrorCounter = false;
-  let repeatedTagErrorCounter = false;
-
-  inputArray.forEach((item, index, array) => {
-    for (let i = index; i < (arrayLength - 1); i++) {
-      if (item === array[i + 1]) {
-        return repeatedTagErrorCounter = true;
-      }
-    }
-  });
-
-  if (repeatedTagErrorCounter) {
-    return 'Хеш-теги не должны повторяться';
-  }
+  const patternErrorCounter = inputArray.some((item) => !REGEXP.test(item));
 
   if (arrayLength > MAX_HASHTAG_NUMBER) {
     return 'Максимальное кол-во хеш-тегов 5';
   }
 
-  inputArray.forEach((item) => {
-    if (!REGEXP.test(item)) {
-      return patternErrorCounter = true;
-    }
-  });
-
   if (patternErrorCounter) {
     return 'Хэш-тег должен начинаться с символа #, без пробелов, содержать только цифры и буквы, и быть не более 20 символов';
   }
 
+  if (new Set(inputArray).size !== arrayLength) {
+    return 'Хеш-теги не должны повторяться';
+  }
   return '';
-}
+};
 
 const hashtagValidityChecker = (evt) => {
   const hashValue = evt.target.value;
-  hashtagsArray = hashValue.split(' ');
-  let errorMessage = hashtagErrorChecker(hashtagsArray);
+  const hashtagsArray = hashValue.split(' ');
+  const errorMessage = hashtagErrorChecker(hashtagsArray);
 
   if (hashValue === '') {
     hashtagInput.setCustomValidity('');
@@ -53,7 +35,7 @@ const hashtagValidityChecker = (evt) => {
   }
 
   hashtagInput.reportValidity();
-}
+};
 
-export { hashtagValidityChecker }
+export { hashtagValidityChecker };
 
